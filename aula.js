0b947699628347fd1b4bf32c89b171646d1e1031
Raquel <raquel@toki.life>
2021-08-03 15:23:42 -0300

    Update aula.js

--- aula.js
+++ aula.js
@@ -1,1 +1,11 @@
+function VerificarEntrada() {
     NomeConvidado = document.getElementById('nome').value;
+    ConvidadosRaquel = [Rafael, Maria Claudia, Jonathan];
+    
+
+    if (ConvidadosRaquel.includes(NomeConvidado)) {
+        document.getElementById('Entrada').innerText = 'Você pode entrar!'
+    } else {
+        document.getElementById('Entrada').innerText = 'Você não pode entrar!'
+    }
+}
